73545f31a39949a911fb7473e0f5488cfc510cef
elliottcable <git@elliottcable.name>
2009-06-22 11:37:58 -0800

    Adding an example File Action, including some source code in a new "Source Code" group.

---
+++ Source Code/File Actions/YRBaseSugarFileTypeDetectingAction.h
@@ -0,0 +1,13 @@
+//
+//  YRBaseSugarFileTypeDetectingAction.h
+//  BaseSugar
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface YRBaseSugarFileTypeDetectingAction : NSObject {
+  NSArray *extensions;
+}
+
+@end
